cce6d96bd86358bbac9022ae352afa9813bdb399
tpichlak <tpichlak@gmail.com>
2021-12-27 19:20:13 +0100

    adding tests for signing up.

--- cypress/integration/initial/initial_spec.ts
+++ cypress/integration/initial/initial_spec.ts
@@ -1,14 +1,44 @@
 /// <reference types="cypress" />
 
-context('Intial', () => {
+context('Sign Up Tests', () => {
   beforeEach(() => {
     cy.visit('http://automationpractice.com')
   })
 
-  it('Sign Up Test', () => {
+  it('Check front page logo', () => {
     cy.get('img.logo.img-responsive')
       .first()
       .invoke('attr', 'src')
       .should('eq', 'http://automationpractice.com/img/logo.jpg')
+  })
+
+  it('Sign Up Test', () => {
+    const currDatesUtcStr = new Date()
+      .toISOString()
+      .replace(':', '-')
+      .replace(':', '-')
+    console.log(currDatesUtcStr)
+
+    cy.get('a.login')
+      .click()
+      .get('#email_create')
+      .type(`test+${currDatesUtcStr}@test.com`)
+      .get('#SubmitCreate')
+      .click()
+
+    cy.get('a.login')
+      .click()
+      .get('#email_create')
+      .type(`test+${currDatesUtcStr}{@test.com`)
+      .get('#SubmitCreate')
+      .click()
+      .get('#create_account_error > ol > li')
+      .first()
+      .should(
+        'have.text',
+        'An account using this email address has already been registered. Please enter a valid password or request a new one. '
+      )
+
+    //create_account_error
   })
 })
